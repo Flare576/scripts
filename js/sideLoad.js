389d9fa18571549855fe309955088d4180125725
Flare576 <flare576@gmail.com>
2019-07-15 10:15:23 -0500

    child process, not new process

--- js/sideLoad.js
+++ js/sideLoad.js
@@ -1,19 +1,17 @@
 const Module = require('module');
 const originalRequire = Module.prototype.require;
-const {execSync, spawn} = originalRequire('child_process');
+const {execSync, spawnSync} = originalRequire('child_process');
 
 Module.prototype.require = function (path) {
     try {
         return originalRequire.apply(this, arguments);
     } catch (e) {
         execSync(`npm -g install ${path}`, { encoding: 'utf8' });
-        process.on("exit", function () {
-            process.argv.shift();
-            spawn(process.argv.shift(), process.argv, {
-                cwd: process.cwd(),
-                detached : true,
-                stdio: "inherit"
-            });
+        process.argv.shift();
+        spawnSync(process.argv.shift(), process.argv, {
+            cwd: process.cwd(),
+            detached : true,
+            stdio: "inherit"
         });
         process.exit();
     }
