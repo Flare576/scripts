591d986754e8e20c3d5f2fd32f7cda69e0def537
Flare576 <flare576@gmail.com>
2019-07-15 12:37:37 -0500

    no longer nested processes, just one active child at a time

--- js/sideLoad.js
+++ js/sideLoad.js
@@ -2,17 +2,31 @@ const Module = require('module');
 const originalRequire = Module.prototype.require;
 const {execSync, spawnSync} = originalRequire('child_process');
 
+const scriptFlag = '--scriptKiddo';
+
 Module.prototype.require = function (path) {
     try {
         return originalRequire.apply(this, arguments);
     } catch (e) {
+        console.log(`Installing ${path}`);
         execSync(`npm -g install ${path}`, { encoding: 'utf8' });
+        const len = process.argv.length;
+        if (len > 1 && process.argv[len - 1] === scriptFlag) {
+            process.exit(42);
+        }
         process.argv.shift();
-        spawnSync(process.argv.shift(), process.argv, {
-            cwd: process.cwd(),
-            detached : true,
-            stdio: "inherit"
-        });
+        const executable = process.argv.shift();
+        process.argv.push(scriptFlag);
+
+        let result = null;
+        while (result === null || result === 42) {
+            result = spawnSync(executable, process.argv, {
+                cwd: process.cwd(),
+                detached : true,
+                stdio: "inherit"
+            });
+            result = result.status;
+        }
         process.exit();
     }
 }
--- js/test.js
+++ js/test.js
@@ -3,3 +3,5 @@
 const argv = require('yargs').argv;
 const inquirer = require('inquirer');
 console.log('main processing unit', argv);
+const answers = inquirer.prompt([{ type:'input', message: 'Iz gud?', name: 'dontcare' }]);
+
